5bba889b397278a9b0c519e9953226e4768b5a8d
TestSubject_8 <agkatreya@gmail.com>
2019-08-23 21:56:41 +0530

    Interrupt based mode change, used with button

--- Interrupt_LED.ino
+++ Interrupt_LED.ino
@@ -1,72 +1,41 @@
-int dim = 2;
+int dim = 3;
 int ind = LED_BUILTIN;
-int bright = 3;
-
-int pot = A6;
-
-int threshold_low = 300;
-int threshold_high = 600; 
-char level;
-int val, vall;
-const int bracket = 10;
-int values[bracket];
-int index = 0;
-int total = 0;
-int avg;
+int bright = 4;
+int changePin = 2;
+volatile int level = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(dim, OUTPUT);
   pinMode(bright, OUTPUT);
   pinMode(ind, OUTPUT);
-  pinMode(pot, INPUT);
+  pinMode(changePin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(changePin),change,RISING);
   Serial.begin(9600);
   for(int i=0;i<bracket;i++){
     values[i]=0;
   }
 }
 
+void change(){
+  level++;
+  level%=3;
+}
+
 void loop() {
-  digitalWrite(ind, HIGH);
-
-  val = analogRead(pot);
-
-  total = total - values[index];
-  total = total + val;
-  avg = total / bracket;
-  values[index] = val;
-  index++;
-  index%=bracket;
-
-  delay(50);
-  vall = 1000 - avg;
-
-  if(vall<threshold_low){
-    level = 'a';
-  }
-  if(vall<threshold_high && vall>=threshold_low){
-    level = 'b';
-  }
-  if(vall>=threshold_high){
-    level = 'c';
-  }
-
-//  Serial.print(val);
-//  Serial.print(" ,");
-//  Serial.print(vall);
-//  Serial.print(" ,");
+//  digitalWrite(ind, HIGH);
   Serial.println(level);
   
   switch(level){
-    case 'a':
+    case 0:
       digitalWrite(dim,LOW);
       digitalWrite(bright,HIGH);
       break;
-    case 'b':
+    case 1:
       digitalWrite(dim,HIGH);
       digitalWrite(bright,LOW);
       break;
-    case 'c':
+    case 2:
       digitalWrite(dim,LOW);
       digitalWrite(bright,LOW);
       break;
